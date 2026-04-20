e2189a03f07af4047d8cc12fc19991fea863289a
saikumar9491 <152430878+saikumar9491@users.noreply.github.com>
2026-04-20 16:45:31 +0530

    update

--- backend/routes/market.route.js
+++ backend/routes/market.route.js
@@ -6,6 +6,9 @@ import { admin } from '../middleware/admin.middleware.js';
 const router = express.Router();
 
 router.get('/prices', protect, getMarketPrices);
+// TEMPORARY PUBLIC DEBUG ROUTE
+router.get('/debug', getMarketPrices);
+
 router.post('/prices', protect, admin, addMarketPrice);
 router.patch('/prices/:id', protect, admin, updateMarketPrice);
 router.delete('/prices/:id', protect, admin, deleteMarketPrice);
